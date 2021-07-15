a28ae073779a7d0806876ceebb354a3926e5eeb3
Caio Cesar <caio.sfg@gmail.com>
2021-07-15 20:33:17 -0300

    Algumas inserções

--- result.rb
+++ result.rb
@@ -1,2 +1,24 @@
 #Exibe bem vindo
-puts "Bem Vindo a sorte de hoje é:"
+nome = "Caio"
+sorte = 13
+combinado = [13,3,22,true, "melhor do dia"]
+
+hash = {"id" => "melanodTTRR080020"}
+
+mensagem = <<~MENSAGEM
+    Olá #{nome}
+    Seja bem vindo(a) sua sorte de hoje é :
+    #{sorte}
+    
+    Aposte Logo !
+
+MENSAGEM
+
+puts mensagem
+puts "-" * 10
+puts mensagem.length
+p combinado
+puts hash[:id]
+puts "Digite sua aposta diária, e tecle enter:"
+aposta = gets
+puts "Aposta - (#{aposta}) realizada pelo código = #{hash[:id]}"
